3bebe042b764b6b5d9f4fcdc4669bd7b1b32ae3a
Jerenmy <93177155+Jerenmy@users.noreply.github.com>
2023-05-15 01:17:32 +0800

    Create xx.py

---
+++ xx.py
@@ -0,0 +1,36 @@
+import turtle
+doraemon = turtle.Turtle()
+
+doraemon.speed(10)
+
+def draw_eye_white_circle(x):
+
+doraemon.goto(x, 80)
+
+doraemon.pendown()
+
+doraemon.color('black')
+
+doraemon.begin_fill()
+
+doraemon.circle(15)
+
+doraemon.color('white')
+
+doraemon.end_fill()
+
+def draw_eye_black_circle(x):
+
+doraemon.goto(x, 90)
+
+doraemon.color('black')
+
+doraemon.begin_fill()
+
+doraemon.begin_fill()
+
+doraemon.circle(6)
+
+doraemon.end_fill()
+
+doraemon.penup()
